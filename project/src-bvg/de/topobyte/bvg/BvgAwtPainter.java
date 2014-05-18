a8a0e6977bc1ab01c58d022dfbdfd45a25431780
Sebastian Kürten <sebastian.kuerten@fu-berlin.de>
2014-05-18 21:22:22 +0200

    Tool for painting to Awt Graphics object

---
+++ project/src-bvg/de/topobyte/bvg/BvgAwtPainter.java
@@ -0,0 +1,77 @@
+// Copyright 2014 Sebastian Kuerten
+//
+// This file is part of bvg.
+//
+// bvg is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// bvg is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with bvg. If not, see <http://www.gnu.org/licenses/>.
+
+package de.topobyte.bvg;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.GeneralPath;
+import java.util.List;
+
+import de.topobyte.bvg.path.CompactPath;
+
+public class BvgAwtPainter
+{
+	public static void draw(Graphics2D g, BvgImage bvg, float x, float y,
+			float sx, float sy)
+	{
+		AffineTransform backup = g.getTransform();
+		g.translate(x, y);
+		g.scale(sx, sy);
+
+		List<PaintElement> elements = bvg.getPaintElements();
+		List<CompactPath> paths = bvg.getPaths();
+
+		for (int i = 0; i < elements.size(); i++) {
+			PaintElement element = elements.get(i);
+			CompactPath path = paths.get(i);
+
+			GeneralPath p = ToSwingUtil.createPath(path);
+
+			if (element instanceof Fill) {
+				Fill fill = (Fill) element;
+				IColor color = fill.getColor();
+				Color c = new Color(color.getRed(), color.getGreen(),
+						color.getBlue(), color.getAlpha());
+
+				g.setColor(c);
+
+				g.fill(p);
+			} else if (element instanceof Stroke) {
+				Stroke stroke = (Stroke) element;
+
+				IColor color = stroke.getColor();
+				Color c = new Color(color.getRed(), color.getGreen(),
+						color.getBlue(), color.getAlpha());
+				g.setColor(c);
+
+				LineStyle lineStyle = stroke.getLineStyle();
+				int cap = ToSwingUtil.getCap(lineStyle.getCap());
+				int join = ToSwingUtil.getJoin(lineStyle.getJoin());
+				BasicStroke bs = new BasicStroke(lineStyle.getWidth(), cap,
+						join, lineStyle.getMiterLimit());
+				g.setStroke(bs);
+
+				g.draw(p);
+			}
+		}
+
+		g.setTransform(backup);
+	}
+}
