76bfe48fae422a936bf8f8384a3586a45662d100
Sebastian Kürten <sebastian@topobyte.de>
2019-12-19 16:24:40 +0100

    Use BvgAwtPainter in BvgToPng

--- util/src/main/java/de/topobyte/bvg/util/BvgToPng.java
+++ util/src/main/java/de/topobyte/bvg/util/BvgToPng.java
@@ -17,32 +17,22 @@
 
 package de.topobyte.bvg.util;
 
-import java.awt.BasicStroke;
-import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
-import java.awt.geom.GeneralPath;
 import java.awt.image.BufferedImage;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.List;
 
 import javax.imageio.ImageIO;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import de.topobyte.bvg.BvgAwtPainter;
 import de.topobyte.bvg.BvgIO;
 import de.topobyte.bvg.BvgImage;
-import de.topobyte.bvg.Fill;
-import de.topobyte.bvg.IColor;
-import de.topobyte.bvg.LineStyle;
-import de.topobyte.bvg.PaintElement;
-import de.topobyte.bvg.Stroke;
-import de.topobyte.bvg.ToSwingUtil;
-import de.topobyte.bvg.path.CompactPath;
 
 public class BvgToPng
 {
@@ -94,61 +84,7 @@ public class BvgToPng
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
 				RenderingHints.VALUE_ANTIALIAS_ON);
 
-		List<PaintElement> elements = bvg.getPaintElements();
-		List<CompactPath> paths = bvg.getPaths();
-
-		for (int i = 0; i < elements.size(); i++) {
-			PaintElement element = elements.get(i);
-			CompactPath path = paths.get(i);
-
-			logger.debug("PATH");
-			GeneralPath p = ToSwingUtil.createPath(path);
-
-			if (element instanceof Fill) {
-				logger.debug("FILL");
-				Fill fill = (Fill) element;
-				IColor color = fill.getColor();
-				logger.debug(String.format("0x%08X", color.getColorCode()));
-				Color c = new Color(color.getRed(), color.getGreen(),
-						color.getBlue(), color.getAlpha());
-
-				g.setColor(c);
-
-				g.fill(p);
-			} else if (element instanceof Stroke) {
-				logger.debug("STROKE");
-				Stroke stroke = (Stroke) element;
-
-				IColor color = stroke.getColor();
-				logger.debug(String.format("0x%08X", color.getColorCode()));
-				Color c = new Color(color.getRed(), color.getGreen(),
-						color.getBlue(), color.getAlpha());
-				g.setColor(c);
-
-				LineStyle lineStyle = stroke.getLineStyle();
-				int cap = ToSwingUtil.getCap(lineStyle.getCap());
-				int join = ToSwingUtil.getJoin(lineStyle.getJoin());
-				float[] dashArray = lineStyle.getDashArray();
-				float dashOffset = lineStyle.getDashOffset();
-
-				BasicStroke bs;
-				if (dashArray == null) {
-					bs = new BasicStroke(lineStyle.getWidth(), cap, join,
-							lineStyle.getMiterLimit());
-				} else {
-					bs = new BasicStroke(lineStyle.getWidth(), cap, join,
-							lineStyle.getMiterLimit(), dashArray, dashOffset);
-				}
-				g.setStroke(bs);
-
-				logger.debug("Linewidth: " + lineStyle.getWidth());
-				logger.debug("Join: " + lineStyle.getJoin());
-				logger.debug("Cap: " + lineStyle.getCap());
-				logger.debug("Miter limit: " + lineStyle.getMiterLimit());
-
-				g.draw(p);
-			}
-		}
+		BvgAwtPainter.draw(g, bvg, 0, 0, 1, 1);
 	}
 
 	public void finish(File fileOutput) throws IOException
